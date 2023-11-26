3674b13f9a46beaf7944948271e67f5116bd0983
JCCode288 <jendycaprico@gmail.com>
2023-11-27 01:21:50 +0700

    fix: inject guard into module

--- src/module/line/line.module.ts
+++ src/module/line/line.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { LineService } from './line.service';
 import { LineWebhookModule } from './line-webhook/line-webhook.module';
 import { LineController } from './line.controller';
+import { WebhookGuard } from './webhook-guard/webhook.guard';
 
 @Module({
   imports: [LineWebhookModule],
-  providers: [LineService],
+  providers: [LineService, WebhookGuard],
   controllers: [LineController],
 })
 export class LineModule {}
