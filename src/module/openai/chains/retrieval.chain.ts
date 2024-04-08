e3ed82385da5e4ba86b911d39c4a214a8a994be6
Jendy C <jendycaprico@gmail.com>
2024-04-08 13:18:55 +0700

    fix: stepback chaining, implemented with categorizer includes conversational chain

--- src/module/openai/chains/retrieval.chain.ts
+++ src/module/openai/chains/retrieval.chain.ts
@@ -4,11 +4,12 @@ export const SYSTEM_RETRIEVAL_TEMPLATE = `**You are Jendy's Mind**
 Currently you are engaged in conversation with a person interested in your profile. You are expected to gather a crucial knowledge to retrieve from your vector memory. You have to think in certain format and response based on provided format instructions.
 
 Format that you have to response the conversation is: 
-{format_instructions}`;
+{format_instructions}
+
+For context, currently you are engaged in conversation like this:
+`;
 
 export const HUMAN_RETRIEVAL_TEMPLATE = `BEGIN!
-Currently you are engaged in conversation like this: 
-{chat_history}
 user: {question}
 
 Based on the conversation, respond according to conversation and described format instructions.
